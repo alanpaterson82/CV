58421eddceaa6e260ac5bcc3bcd7577cfd5b937d
alanpaterson82 <>
2023-12-28 14:56:43 +0000

    implement emailjs integration on contact page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -22,5 +22,5 @@ function initMap() {
         });
     });
 
-    var markerCluster = new markerCluster(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
